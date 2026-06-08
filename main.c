93fcdc177bbd1954de00161af8ecfc220cdaaba9
Sinchana N Poojar <sinchananpoojar@gmail.com>
2026-06-08 21:27:08 +0530

    Added canvas initialization

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+#define ROWS 10
+#define COLS 20
+
+char canvas[ROWS][COLS];
+
 int main() {
-    printf("2D Graphics Editor\n");
+    int i, j;
+
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            canvas[i][j] = '_';
+        }
+    }
+
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
